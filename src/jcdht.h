179636eae740b871eebda6d68b9eba45f35a631f
loadletter <loadletter@users.noreply.github.com>
2014-05-07 21:22:24 +0200

    Added header

---
+++ src/jcdht.h
@@ -0,0 +1,23 @@
+#ifndef JCDHT_H
+#define JCDHT_H
+
+#include <Python.h>
+#include "dht/dht.h"
+
+typedef struct {
+	int s, s6, port;
+	int have_id;
+	unsigned char myid[20];
+	time_t tosleep;
+	int ipv4, ipv6;
+} DHT;
+
+typedef struct {
+	PyObject_HEAD
+	DHT* dht;
+} JCDHT;
+
+void JCDHT_install_dict(void);
+
+#endif /* JCDHT_H */
+
